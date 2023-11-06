a7ec7f4786e72b6d4a1fa2418b84d3c62fbf945b
Tom Scharstein <trscharstein@gmail.com>
2023-11-05 22:42:15 -0700

    Adding catchall wildcard route redirect

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,10 @@ const router = createRouter({
       name: "tools",
       component: () => import("../views/Tools.vue"),
     },
+    {
+      path: "/:catchAll(.*)*",
+      component: () => import("../views/Tools.vue"),
+    },
   ],
 });
 
